0a4a4da8740c8c118dc4b2323371a25984bf6e04
mrjlancaster <lancast001@gmail.com>
2020-09-19 22:17:24 -0500

    delete hero img import from index js

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
-import './img/hero.jpg';
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
