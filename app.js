55eee4aefeab851994d3ebf0e10ba68e1e4a7e6a
mrjlancaster <lancast001@gmail.com>
2020-11-16 18:33:07 -0600

    setup database

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
-
-// Create express app
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+const Post = require('./models/Posts');
 
-// Run server
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
-});
+// testing route
+app.get('/posts', (req, res) => {
+	const post = new Post({
+		title: 'Coronavirus',
+		description: 'briefly talking about corona'
+	});
+	post.save()
+		 .then(item => {
+			  res.send(item)
+		 })
+})
+
+// Connect to DB
+mongoose.connect(process.env.DB_POSTS, { useNewUrlParser: true }, () => {
+	// connection success message
+	console.log('Connected to MongoDB successfully!');
+	
+	// Run server
+	app.listen(3000, () => {
+		console.log('Server running on port 3000');
+	})
+})
